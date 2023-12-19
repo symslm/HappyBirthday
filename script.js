d1479271926dfcd9c7c741cb762bb2dd7b1672b1
Sayem Salim <141453083+symslm@users.noreply.github.com>
2023-12-19 08:09:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, Curse have been Accepted Purba Swain";
+  question.innerHTML = "Yay, Curse Response have been submitted to Intergalactic-Cosmos Purba Swain";
   gif.src =
     "https://media1.giphy.com/media/JErAGpPtUV8ze/giphy.gif?cid=ecf05e479czwzmu06y9ow89vtz10hj2fi7g3u9yesm8vfe7m&ep=v1_gifs_related&rid=giphy.gif&ct=g";
 });
